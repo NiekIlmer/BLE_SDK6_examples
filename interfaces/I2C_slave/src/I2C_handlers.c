3a65fb3d02281541b1889b104f1b03937f784374
Niek Ilmer <Niek.Ilmer@diasemi.com>
2021-06-10 14:31:52 +0200

    I2C example WIP

---
+++ interfaces/I2C_slave/src/I2C_handlers.c
@@ -0,0 +1,11 @@
+
+#include "I2C_handlers.h"
+
+void I2C_Start_handler(uint16_t mask){
+	uint16_t volatile raw = i2c_get_raw_int_state();
+}
+
+
+
+
+
---
+++ interfaces/I2C_slave/src/I2C_handlers.h
@@ -0,0 +1,4 @@
+
+#include "i2c.h"
+void I2C_Start_handler(uint16_t mask);
+
